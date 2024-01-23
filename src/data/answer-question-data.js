9923311466e0e5fd51794ee29925a3979d600170
Faizullah-Laghari <faizullahls123@gmail.com>
2024-01-23 18:02:21 +0500

    initial commit

---
+++ src/data/answer-question-data.js
@@ -0,0 +1,60 @@
+const answer_question_data = [
+  {
+    id: 1,
+    question: "Why should I choose Cyber Secure",
+    answer: <>The  Cyber Secure is built right into your account dashboard, and is accessible
+          immediately after signing up. </> ,
+    accordion_id: "headingOne",
+    collapsed: "collapsed",
+    data_bs_target: "#collapseOne",
+    aria_controls: "collapseOne", 
+    actice: "",
+  },
+  {
+    id: 2,
+    question: "What is  Cyber Secure and how does it benefit me?",
+    answer: <> Cyber Secure is a powerful feature integrated into your Swift account, providing a suite of tools and functionalities to streamline your business operations. It offers efficiency, convenience, and customization to enhance your overall experience.</> ,
+    accordion_id: "headingTwo",
+    collapsed: "",
+    data_bs_target: "#collapseTwo",
+    aria_expanded: true,
+    aria_controls: "collapseTwo",
+    show: true,
+    actice: "",
+
+  },
+  {
+    id: 3,
+    question: "How do I get started with  Cyber Secure?",
+    answer: <>Getting started with  Cyber Secure is easy! Upon signing up for Softec, Swift Business is readily available in your account dashboard. Simply navigate to the designated section to explore its features and functionalities.</> ,
+    accordion_id: "headingThree",
+    collapsed: "collapsed",
+    data_bs_target: "#collapseThree",
+    aria_controls: "collapseThree",
+    actice: "",
+
+  },
+  {
+    id: 4,
+    question: "What specific tools does  Cyber Secure include?",
+    answer: <> Cyber Secure is equipped with a diverse set of tools designed to address various aspects of business management. From project tracking to communication features, Swift Business empowers you with the resources you need for success.</> ,
+    accordion_id: "headingFour",
+    collapsed: "collapsed",
+    data_bs_target: "#collapseFour",
+    aria_controls: "collapseFour", 
+    actice: "",
+
+  },
+  {
+    id: 5,
+    question: "Can I integrate  Cyber Secure with other applications?",
+    answer: <>Yes,  Cyber Secure is designed to be flexible and integrative. You can seamlessly connect it with other applications and tools to create a cohesive and efficient workflow that suits your business requirements.</> ,
+    accordion_id: "headingFive",
+    collapsed: "collapsed",
+    data_bs_target: "#collapseFive",
+    aria_controls: "collapseFive", 
+    actice: "",
+
+  }, 
+];
+export default answer_question_data;
